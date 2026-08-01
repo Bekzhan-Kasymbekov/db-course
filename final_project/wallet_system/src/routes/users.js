599dd5524a265c72d99bee8c2452a03002f20c0b
bekzhan <kasymbekov_be@auca.kg>
2026-08-02 01:58:01 +0600

    Protect user routes with Keycloak roles

--- final_project/wallet_system/src/routes/users.js
+++ final_project/wallet_system/src/routes/users.js
@@ -2,44 +2,56 @@ const express = require('express');
 
 const user_service = require('../services/userService');
 const { is_valid_email } = require('../utils/validators');
+const authenticate = require('../middlewares/authenticate');
+const {
+    require_realm_role,
+} = require('../middlewares/authorize');
 
 const router = express.Router();
 
-router.post('/', async (req, res, next) => {
-    try {
-        const {email} = req.body;
+router.post(
+    '/', 
+    authenticate,
+    require_realm_role('wallet_admin'),
+    async (req, res, next) => {
+        try {
+            const { email } = req.body;
 
-        if (!is_valid_email(email)) {
-            const error = new Error('Valid email is required');
-            error.status_code = 400;
-            throw error;
+            if (!is_valid_email(email)) {
+                const error = new Error('Valid email is required');
+                error.status_code = 400;
+                throw error;
+            }
+
+            const user = await user_service.create_user(email);
+
+            res.status(201).json({
+                user,
+            });
+        } catch (error) {
+            if (error.code === '23505') {
+                error.status_code = 409;
+                error.message = 'User with this email already exists';
+            }
+
+            next(error);
         }
-
-        const user = await user_service.create_user(email);
-
-        res.status(201).json({
-            user,
-        });
-    } catch (error) {
-        if (error.code === '23505') {
-            error.status_code = 409;
-            error.message = 'User with this email already exists';
-        }
-
-        next(error);
-    }
 });
 
-router.get('/', async (req, res, next) => {
-    try {
-        const users = await user_service.get_users();
+router.get(
+    '/',
+    authenticate,
+    require_realm_role('wallet_admin', 'wallet_auditor'),
+    async (req, res, next) => {
+        try {
+            const users = await user_service.get_users();
 
-        res.json({
-            users,
-        });
-    } catch (error) {
-        next(error)
-    }
+            res.json({
+                users,
+            });
+        } catch (error) {
+            next(error)
+        }
 });
 
 module.exports = router;
